307f329fa21434e6e2c20eb3aceb5829d666dd46
Lakshya Manan Jaiswal <lakshyamanan2277@gmail.com>
2021-03-08 17:05:42 +0530

    Initial commit

---
+++ android/app/src/main/java/com/experiments/wsd_lab/MainActivity.java
@@ -0,0 +1,6 @@
+package com.experiments.wsd_lab;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('Work Systems Design Lab'),
+      centerTitle: true,
+      backgroundColor: Colors.lightGreenAccent[700],
+    ),
+    body: Center(
+      child: Text("Caution! Work in Progress\nLogo color hex code:8cc63f",
+        style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.5,
+            color: Colors.grey[600]
+        ),
+      ),
+
+    )
+
+  ),
+
+));
