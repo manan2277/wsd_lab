23b644d341afbd5c46e89d0acee6f378c0d1579d
Lakshya Manan Jaiswal <lakshyamanan2277@gmail.com>
2021-03-09 17:35:46 +0530

    Test Change to edit title

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-            title: Text('Work Systems Lab'),
+            title: Text('Work Lab'),
             centerTitle: true,
             backgroundColor: Colors.lightGreenAccent[700],
           ),
