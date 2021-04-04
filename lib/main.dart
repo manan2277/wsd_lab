b1b2e4f8212868caa244cad76b850ebeb604b6ba
nerellasaikiran <nerellasaikiran5287@gmail.com>
2021-04-04 12:55:39 +0530

    added_quiz_code

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'question.dart';
 
 void main() => runApp(MaterialApp(
       home: Home(),
@@ -189,6 +190,19 @@ class SubPage extends StatelessWidget {
               },
               child: Text(
                 "Anthropometry: Measuring and analyzing data for the group of subjects in  different postures",
+                style: TextStyle(fontSize: 20, color: Colors.blue),
+              ),
+              color: Colors.white,
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Quiz()),
+                );
+              },
+              child: Text(
+                "QUIZ",
                 style: TextStyle(fontSize: 20, color: Colors.blue),
               ),
               color: Colors.white,
@@ -1180,6 +1194,244 @@ class Anthro extends StatelessWidget {
         onPressed: () {
           Navigator.pop(context);
         },
+      ),
+    );
+  }
+}
+
+class Quiz extends StatefulWidget {
+  @override
+  _QuizState createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          'QUIZ',
+          style: TextStyle(
+              fontSize: 22, fontWeight: FontWeight.bold, color: Colors.red[50]),
+        ),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: StartPage(),
+      ),
+    );
+  }
+}
+
+class StartPage extends StatefulWidget {
+  @override
+  _StartPageState createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  var counter = 0;
+
+  List qList = [
+    Questions(
+        "1. Class C fires involve energized\n    electrical equipment.", true),
+    Questions(
+        "2. The wet chemical extinguisher is a  \n    specialized type primarily focused \n    on class K fires.",
+        true),
+    Questions(
+        "3. Hypothermia can occur if you \n    immerse large, severe burns in\n    water.",
+        true),
+    Questions(
+        "4. DIAL 101 for fire brigade,102 for      \n    ambulance.", true),
+    Questions(
+        "5. Foam fire extinguishers are \n    suitablefor class A .", true),
+    Questions(
+        "6. A burn is tissue damage from \n    contactwith flames.", true),
+    Questions(
+        "7. Aim the nozzle or hose at the base \n    of the fire from the recommended \n    safe distance.",
+        true),
+    Questions(
+        "8. A water mist extinguisher is safe \n    and effective for use on classes A,\n    B, C and K fires.",
+        true),
+    Questions(
+        "9. AFCI's are used to shut off\n    electricity in dangerous condition.",
+        true),
+    Questions(
+        "10. A clean agent fire extinguisher  is\n     a type of gaseous fire\n     suppression. Stored in its liquid\n     form.",
+        true),
+  ];
+
+  var score = 0;
+
+  checkWin(bool userChoice, BuildContext context) {
+    if (userChoice == qList[counter].isCorrect) {
+      print("correct");
+
+      score = score + 5;
+      if (score >= 50) {
+        score = 50;
+      }
+      final snackbar = SnackBar(
+        duration: Duration(milliseconds: 1000),
+        backgroundColor: Colors.green,
+        content: Text("Correct!"),
+      );
+      Scaffold.of(context).showSnackBar(snackbar);
+    } else {
+      print("false");
+      score = score + 0;
+
+      final snackbar = SnackBar(
+        duration: Duration(milliseconds: 1000),
+        backgroundColor: Colors.red,
+        content: Text("Incorrect!"),
+      );
+      Scaffold.of(context).showSnackBar(snackbar);
+    }
+    setState(() {
+      if (counter < 9) {
+        counter = counter + 1;
+      }
+    });
+  }
+
+  reset() {
+    setState(() {
+      counter = 0;
+      score = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber[50],
+      body: Builder(
+        builder: (BuildContext context) => Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: 270,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('images/pic3.png'), fit: BoxFit.fill),
+                ),
+              ),
+              //Padding(padding: EdgeInsets.only(top: 30)),
+              Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                      Color(0xFFfcd744),
+                      Color(0xFFfcd744),
+                    ])),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text(
+                      "Score : $score /50",
+                      style: TextStyle(
+                          color: Colors.green,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    InkWell(
+                      child: Text(
+                        "Reset Quiz",
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.redAccent,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      onTap: reset,
+                    )
+                  ],
+                ),
+              ),
+              //Padding(padding: EdgeInsets.only(top: 30)),
+              Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                      Color(0xFFfcd745),
+                      Color(0xFFe8c752),
+                      Colors.amber[50]
+                    ])),
+                height: 150.0,
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    RaisedButton(
+                      onPressed: () => {},
+                      padding: EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 20.0),
+                      child: Text(qList[counter].qText,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.green,
+                          )),
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          side: BorderSide(
+                            color: Colors.green,
+                            width: 3,
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(top: 30)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: () => checkWin(true, context),
+                    padding: EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 20.0),
+                    child: Text(
+                      "TRUE",
+                      style: TextStyle(
+                          color: Colors.green,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        side: BorderSide(
+                          color: Colors.green,
+                          width: 3,
+                        )),
+                  ),
+                  RaisedButton(
+                    onPressed: () => checkWin(false, context),
+                    padding: EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 20.0),
+                    child: Text(
+                      "FALSE",
+                      style: TextStyle(
+                          color: Colors.redAccent,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        side: BorderSide(
+                          color: Colors.redAccent,
+                          width: 3,
+                        )),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
